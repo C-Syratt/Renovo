0b715d91b9d61e7ed72960b919d950665eeed047
C-Syratt <c.syratt@aanet.com.au>
2014-10-08 20:00:24 +1000

    Making Preparations for BirdsEye view
    + added camView enum // tells the camera what modes it's in
    + separated camera rotations from character movement

--- unity/Assets/FPC.cs
+++ unity/Assets/FPC.cs
@@ -5,16 +5,19 @@ public class FPC : MonoBehaviour {
 
 
 	CharacterController charC;
+	public GameObject player;
+	[SerializeField]camScript cam;
 
 	//public AudioClip jumpAud;
 
 	public float moveSpeed = 5.0f;
-	public float mouseSensitivity = 2.0f;
 	public float jumpSpeed = 5.0f;
-	public float upDownRange = 50.0f;
-	// sadfasdfa
 
-	float vertRot = 0;
+//	public float mouseSensitivity = 2.0f;
+//	public float upDownRange = 50.0f;
+//	// sadfasdfa
+//
+//	float vertRot = 0;
 	float vertVelo = 0;
 
 	// Use this for initialization
@@ -22,20 +25,21 @@ public class FPC : MonoBehaviour {
 	{
 		Screen.lockCursor = true;
 		charC = GetComponent<CharacterController> ();
+		player = this.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float yaw = Input.GetAxis("Mouse X") * mouseSensitivity;
-		transform.Rotate (0, yaw, 0);
-		vertRot -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
-
-		// more shit about stuff.rg78oashud fg7yukhqwef
-		vertRot = Mathf.Clamp (vertRot, -upDownRange, upDownRange);
-		Camera.main.transform.localRotation = Quaternion.Euler(vertRot, 0, 0);
-
-
+//		float yaw = Input.GetAxis("Mouse X") * mouseSensitivity;
+//		transform.Rotate (0, yaw, 0);
+//		vertRot -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+//
+//		// more shit about stuff.rg78oashud fg7yukhqwef
+//		vertRot = Mathf.Clamp (vertRot, -upDownRange, upDownRange);
+//		Camera.main.transform.localRotation = Quaternion.Euler(vertRot, 0, 0);
+//
+//
 		// increase velocity the longer you fall
 		vertVelo += Physics.gravity.y * Time.deltaTime;
 
---
+++ unity/Assets/camScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camScript : MonoBehaviour {
+
+	public enum camView
+		{
+			thirdPerson,
+			birdsEye
+		}
+
+	public GameObject player;
+	public camView view;
+	public float mouseSensitivity = 2.0f;
+	float vertRot = 0;
+	public float upDownRange = 50.0f;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (view == camView.thirdPerson)
+			ThirdPerson ();
+
+		else if (view == camView.birdsEye)
+			BirdsEye ();
+
+	}
+
+	void ThirdPerson()
+	{
+		float yaw = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		player.transform.Rotate (0, yaw, 0);
+		vertRot -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		// more shit about stuff
+		vertRot = Mathf.Clamp (vertRot, -upDownRange, upDownRange);
+		transform.localRotation = Quaternion.Euler (vertRot, 0, 0);
+	}
+
+	void BirdsEye()
+	{
+
+	}
+
+
+	public void changeView()
+	{
+		if (view == camView.thirdPerson)
+			view = camView.birdsEye;
+
+		else if (view == camView.birdsEye)
+			view = camView.thirdPerson;
+
+	}
+
+
+
+
+
+}
