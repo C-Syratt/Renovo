a039b42c53dd1c13c0938ce4d5b5af1d9e73ce98
C-Syratt <c.syratt@aanet.com.au>
2014-10-09 21:38:09 +1000

    Something
    For some reason Github didn't include this in the commit or something

---
+++ unity/Assets/pageCam.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pageCam : MonoBehaviour 
+{
+	public static pageCam pc;
+
+
+	void Start()
+	{
+		pc = gameObject.GetComponent<pageCam> ();
+	}
+
+	public static void TurnCamOn(string camNum)
+	{
+		foreach(Transform child in transform)
+		{
+			if(child.name == camNum) //Camera One
+			{
+				child.gameObject.SetActive(true);
+			}
+			if(child.name == "CamTwo")
+			{
+				child.gameObject.SetActive(true);
+			}
+			if(child.name == "CamThree")
+			{
+				child.gameObject.SetActive(true);
+			}
+		}
+	}
+
+
+
+
+
+}
