7ae3e9fde0555ec816a6ae938b4fe619c2876326
C-Syratt <c.syratt@aanet.com.au>
2014-10-10 21:47:27 +1000

    Final Puzzle
    + Added final puzzle + scripts // Camera's still need to be set up
    + Added Page .png's to assets folder

---
+++ unity/Assets/Scripts/DialPuzzle.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialPuzzle : MonoBehaviour 
+{
+	public GameObject[] dialList;
+	public GameObject selectedDial;
+	public float rotSpeed;
+	public int dialNum = 0;
+
+	public Color selectedColor;
+	public Color originalColor;
+
+	public void Start()
+	{
+		selectedDial = dialList[0];
+		originalColor = dialList[1].renderer.material.color;
+	}
+
+	public void Update()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{RotateLeft ();}
+
+		if (Input.GetKey (KeyCode.RightArrow))
+		{RotateRight ();}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+		{ChangeUp ();}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow))
+		{ChangeDown ();}
+
+//		if (selectedDial) 
+//		{
+//			selectedDial.renderer.material.color = selectedColor;
+//		}
+//		else
+//		{
+//			renderer.material.color = originalColor;
+//		}
+
+		selectedDial.renderer.material.color = selectedColor;
+
+		for (int i = 0; i < dialList.Length; i++) 
+		{
+			if (i != dialNum)
+			{
+				dialList[i].renderer.material.color = originalColor;
+			}
+		}
+	}
+
+	void RotateLeft()
+	{
+		selectedDial.gameObject.transform.Rotate (new Vector3 (0, -1, 0) * rotSpeed);
+	}
+
+	void RotateRight()
+	{
+		selectedDial.gameObject.transform.Rotate (new Vector3 (0, 1, 0) * rotSpeed);
+	}
+
+	void ChangeUp()
+	{
+		if (selectedDial == dialList[2])
+		{
+			selectedDial = dialList [0];	
+			dialNum = 0;
+		}
+		else
+		{
+			selectedDial = dialList [dialNum + 1];
+			dialNum++;
+		}
+	}
+
+	void ChangeDown()
+	{
+		if (selectedDial == dialList[0])
+		{
+			selectedDial = dialList [2];	
+			dialNum = 2;
+		}
+		else
+		{
+			selectedDial = dialList [dialNum - 1];
+			dialNum--;
+		}
+	}
+
+}
