a53a8ca69c41c316df34fba22340fd4620e3ec3d
C-Syratt <c.syratt@aanet.com.au>
2014-10-14 10:32:20 +1000

    Main Menu Functionality
    + Added Instructions screen to menu
    + Made Menu Buttons Functional
    + Added Menu and Game scene to Build Settings
    + End Game now resets level (Camera fix still needed)

---
+++ unity/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	[SerializeField]GameObject cam;
+
+	void OnMouseDown()
+	{
+		switch(gameObject.name)
+		{
+		case "Start":
+			Application.LoadLevel(1);
+			break;
+
+		case "Back":
+			cam.SendMessage("MoveCam", "Back");
+			break;
+
+		case "Instructions":
+			cam.SendMessage("MoveCam", "Instruct");
+			break;
+		
+		}
+	}
+}
---
+++ unity/Assets/Scripts/MenuCam.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCam : MonoBehaviour {
+
+	[SerializeField]float camSpeed;
+		
+	Vector3 newPos;
+	Vector3 mainPos = new Vector3(26f,0.25f,2.12f);
+	Vector3 controlPos = new Vector3(26f,7.61f,-0.5771289f);
+
+	
+	void Start () 
+	{
+		newPos = mainPos;
+	}
+	
+	void Update () 
+	{
+		transform.position = Vector3.MoveTowards(transform.position, newPos, camSpeed * Time.deltaTime);
+	}
+	
+	void MoveCam(string buttonPressed)
+	{
+		switch (buttonPressed) 
+		{
+		case "Instruct":
+			newPos = controlPos;
+			break;
+			
+		case "Credits":
+			//
+			break;
+			
+		case "Back":
+			newPos = mainPos;
+			break;
+			
+		}
+	}
+}
